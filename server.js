ff209c262e998ea94e4a2bb06594c219b0e3321d
Marta Fonda <martafondapascual@gmail.com>
2014-12-08 17:43:06 +0100

    Adding AngularJS, controller and model done. Style and javascript menu bar

--- server.js
+++ server.js
@@ -78,6 +78,11 @@ app.delete('/api/todos/:todo_id', function(req, res) {
     });
 });
 
+// application -------------------------------------------------------------
+    app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+    
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
